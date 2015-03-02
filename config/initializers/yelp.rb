ab18e62275000e12531f20f222694986266a17ef
RahulKhanna <sociallyrk@gmail.com>
2015-03-02 13:40:37 -0800

    yelp rails working

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,7 +1,7 @@
 Yelp.client.configure do |config|
-  config.consumer_key = ENV['i6HWG0XTZFSlDDN6lVS4mw']
-  config.consumer_secret = ENV['fmk_Em2pJ77hpE-pPj2AKMtLNbk']
-  config.token = ENV['nu4G-vC12-wHA8mO-ryoJkOkES15dEp9']
-  config.token_secret = ENV['anz17cUc48TLpZXKFQ5yTDHA0Ek']
+  config.consumer_key = 'i6HWG0XTZFSlDDN6lVS4mw'
+  config.consumer_secret = 'fmk_Em2pJ77hpE-pPj2AKMtLNbk'
+  config.token = 'nu4G-vC12-wHA8mO-ryoJkOkES15dEp9'
+  config.token_secret = 'anz17cUc48TLpZXKFQ5yTDHA0Ek'
 end
 
