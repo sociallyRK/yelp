eba106e641ec6045c15352461134b03bb09e6844
RahulKhanna <sociallyrk@gmail.com>
2015-03-01 16:05:02 -0800

    latest

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,6 +1,7 @@
 Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
+  config.consumer_key = ENV['i6HWG0XTZFSlDDN6lVS4mw']
+  config.consumer_secret = ENV['fmk_Em2pJ77hpE-pPj2AKMtLNbk']
+  config.token = ENV['nu4G-vC12-wHA8mO-ryoJkOkES15dEp9']
+  config.token_secret = ENV['anz17cUc48TLpZXKFQ5yTDHA0Ek']
 end
+
